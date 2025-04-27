4b75eeeab178c0fc5a4388d484d5acf1bb01c133
notlemos <caiolemossfc123@hotmail.com>
2025-04-26 22:18:03 -0300

    feat: better spotify status

--- src/cogs/spotify.py
+++ src/cogs/spotify.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord.activity import Spotify
 import logging
 import requests
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 from src.utils.rounded_field import draw_rounded_field_with_alpha
 from io import BytesIO
 logger = logging.getLogger(__name__)
@@ -17,7 +17,7 @@ class SpotifyPillow(commands.Cog):
     @commands.command(name="spotify")
     async def spotify(self, ctx, user: discord.Member = None):
         user = user or ctx.author 
-        image = Image.open('backgrounds/spotifybg3.png').convert("RGBA")
+        image = Image.open('backgrounds/spotifybg.png').convert("RGBA")
         
 
         for activity in user.activities:
@@ -38,29 +38,43 @@ class SpotifyPillow(commands.Cog):
 
                 # Salvando a capa do Album.
                 
-                url = activity.album_cover_url 
+                url = activity.album_cover_url
                 response = requests.get(url)
-                capa = Image.open(BytesIO(response.content)).resize((180,180)).convert("RGBA")
-                radius = 20
-                pos_x, pos_y = 600, 20
+                capa = Image.open(BytesIO(response.content)).resize((378, 364), Image.Resampling.LANCZOS)
 
+                # Posições para colar a imagem
+                pos_x, pos_y = 72, 42
 
-            
-
-                # Arredondar a capa
+                # Criar a máscara com bordas arredondadas
                 mask = Image.new("L", capa.size, 0)
                 draw = ImageDraw.Draw(mask)
-                draw.rounded_rectangle((0, 0, 180, 180), radius=radius, fill=255)
+
+                # Criar uma borda arredondada com transição suave
+                radius = 15
+                draw.rounded_rectangle((0, 0, 378, 364), radius=radius, fill=255)
+
+                # Melhorar a suavização das bordas aplicando um filtro de desfoque mais leve
+                #mask = mask.filter(ImageFilter.GaussianBlur(5))  # Ajuste o valor conforme necessário
+
+                # Criar a imagem com fundo transparente
                 capa_arredondada = Image.new("RGBA", capa.size)
                 capa_arredondada.paste(capa, (0, 0), mask)
+
+                # Agora, aplique uma suavização à própria imagem para suavizar as bordas mais visíveis (caso necessário)
+                capa_arredondada = capa_arredondada.filter(ImageFilter.SMOOTH_MORE)
+
+                # Colocar a máscara na imagem arredondada
+                capa_arredondada.putalpha(mask)
+
+                # Colar a imagem arredondada na imagem final
                 
                 
                     
                 # Fontes Escritas do spotify
-                font_nome_user = ImageFont.truetype("fonts/Roboto-Regular.ttf", 30)
-                font_main = ImageFont.truetype("fonts/Roboto-Regular.ttf", 23)
-                font_menor = ImageFont.truetype("fonts/Roboto-Regular.ttf", 18)
-                font_tempo = ImageFont.truetype("fonts/Roboto-Regular.ttf", 14)
+                font_nome_user = ImageFont.truetype("fonts/BebasNeue-Regular.otf", 42)
+                font_main = ImageFont.truetype("fonts/BebasNeue-Regular.otf", 48)
+                font_menor = ImageFont.truetype("fonts/Roboto-Regular.ttf", 28)
+                font_tempo = ImageFont.truetype("fonts/BebasNeue-Regular.otf", 26)
                 # Campos de escrita.
                 
                 user_name = ImageDraw.Draw(image)
@@ -68,24 +82,23 @@ class SpotifyPillow(commands.Cog):
                 album_name = ImageDraw.Draw(image)
                 artists_name = ImageDraw.Draw(image)
                 
-                rights = ImageDraw.Draw(image)
+                
                 
                 tempo = ImageDraw.Draw(image)
                 # Campo do nome
-                draw_rounded_field_with_alpha(image,position=(50, 20),size=(500, 50),radius=10,fill_color=(255, 255, 255, 20) ) # Transparente! 
-                user_name.text((80,25), f"{user.name}'s spotify", font=font_nome_user, fill=(250,250,250,250))
                 
-                draw_rounded_field_with_alpha(image,position=(30, 90),size=(520, 190),radius=10,fill_color=(255, 255, 255,20) ) # Transparente! 
-                music_name.text((60, 100), f"Music", font=font_main, fill=(250,250,250,250))
-                music_name.text((63, 130), f"{activity.title}", font=font_menor, fill=(250,250,250,250))
+                user_name.text((492,352), f"{user.name}'s spotify", font=font_nome_user, fill=(250,250,250))
                 
-                album_name.text((60,160), "Album", font=font_main, fill=(250,250,250,250))
-                album_name.text((63, 190), f"{activity.album}", font=font_menor, fill=(250,250,250,250))
-                
-                artists_name.text((60,220), "Artists", font=font_main, fill=(250,250,250,250))
-                artists_name.text((63, 250), f"{activity.artist}", font=font_menor, fill=(250,250,250,250))
-                
-                rights.text((490, 73), f"notlemos", font=font_tempo, fill=(255, 255, 255, 100))
+               
+                music_name.text((88, 520), "Music", font=font_main, fill=(250,250,250))
+                music_name.text((94, 590), activity.title, font=font_menor, fill=(250,250,250))
+
+                album_name.text((88, 660), "Album", font=font_main, fill=(250,250,250))
+                album_name.text((94, 730), activity.album, font=font_menor, fill=(250,250,250))
+
+                artists_name.text((88, 800), "Artists", font=font_main, fill=(250,250,250))
+                artists_name.text((94, 870), activity.artist, font=font_menor, fill=(250,250,250))
+                                                
                 
                 
 
@@ -97,13 +110,12 @@ class SpotifyPillow(commands.Cog):
                 
                 elapsed_str = format_timedelta(elapsed_time)
                 duration_str = format_timedelta(duration)
-                icon = Image.open("backgrounds/spotify.png").resize((60,60))
-                image.paste(icon, (660, 230), icon)
-                tempo.text((650, 210), f"  {elapsed_str} / {duration_str}", font=font_tempo, fill=(250,250,250,250))
+                
+                tempo.text((655, 210), f"  {elapsed_str} / {duration_str}", font=font_tempo, fill=(250,250,250,250))
                 
                 image.paste(capa_arredondada, (pos_x, pos_y), capa_arredondada)
 
-                image.save("spotify_card.png")
+                image.save("spotify_card.png", format="png", optimize=True)
                 file = discord.File("spotify_card.png", filename="spotify_card.png")
                 await ctx.send(file=file)
                 return
